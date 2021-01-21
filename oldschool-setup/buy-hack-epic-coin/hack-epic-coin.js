b6ba7fd6232746e3b394f2c336aaf6b3656d7016
Shawn <45756873+shawn-p-m@users.noreply.github.com>
2021-01-21 09:15:13 -0800

    Good enough xD

--- oldschool-setup/buy-hack-epic-coin/hack-epic-coin.js
+++ oldschool-setup/buy-hack-epic-coin/hack-epic-coin.js
@@ -1,12 +1,31 @@
-var i = 0;
+var items = 0;
 function incrementCart() {
 
-    document.getElementById('item-quantity').value = ++i;
+    document.getElementById('item-quantity').value = ++items;
 
 }
 
 function decrementCart() {
-    if (i > 0)
-        document.getElementById('item-quantity').value = --i;
+    if (items > 0)
+        document.getElementById('item-quantity').value = --items;
 
+}
+
+function buyNow() {
+    if (items > 0) {
+
+        document.getElementById('credit-card').style = "display:inherit";
+        document.getElementById('words').style = "display:none";
+    }
+
+    else {
+        alert("Please add some coins to your cart!")
+    }
+}
+
+function submitCard() {
+
+    alert("We got your card now sucker! <3")
+    alert("It's " + document.getElementById('card-number').value)
+    window.location.href = "../";
 }
