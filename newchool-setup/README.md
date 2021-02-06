495af2f9485940808a127be5f5279034c64f1e25
Shawn <45756873+shawn-p-m@users.noreply.github.com>
2021-02-05 17:37:08 -0800

    add newschool setup

---
+++ newchool-setup/README.md
@@ -0,0 +1,30 @@
+# NewSchool Web Project VueJS
+
+## **JS Specifics**
+
+
+- map, reduce, prototypes/ inheritance, async await, promises, generators, mixins, objects, datatypes, debugging, spread
+
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn serve
+```
+
+### Compiles and minifies for production
+```
+yarn build
+```
+
+### Lints and fixes files
+```
+yarn lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ newchool-setup/src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <div id="app">
+    <v-app>
+      <v-container fluid>
+        <v-row>
+          <v-col md="4"></v-col>
+          <v-col md="4">
+            <img class="d-flex justify-center" alt="Vue logo" src="./assets/logo.png" />
+          </v-col>
+          <v-col md="4"></v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <v-dialog v-model="dialog" width="500">
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn class="d-flex justify-center" color="info" v-bind="attrs" v-on="on">
+                  Instructions
+                </v-btn>
+              </template>
+
+              <v-card>
+                <v-card-title class="headline grey lighten-2"> Instructions </v-card-title>
+
+                <v-card-text
+                  ><br />
+                  Click the buttons below after implementing the functions inside
+                  src/mixins/specifics.js to check if you implemented them correctly!
+                </v-card-text>
+
+                <v-divider></v-divider>
+
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="secondary" text @click="dialog = false"> Got it! 😎 </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+          </v-col>
+        </v-row>
+        <FunctionButtons />
+      </v-container>
+    </v-app>
+  </div>
+</template>
+
+<script>
+import FunctionButtons from "@/components/FunctionButtons";
+export default {
+  name: "App",
+  components: { FunctionButtons },
+  data() {
+    return {
+      dialog: false,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+img {
+  width: 200px;
+  margin: 5px auto;
+}
+.v-btn {
+  width: 150px;
+  margin: 30px auto !important;
+}
+</style>
